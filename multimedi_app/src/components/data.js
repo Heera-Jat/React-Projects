af38baf7976159222511657bcf13b0a002914d3c
Heera Jat <106680359+Heera-Jat@users.noreply.github.com>
2023-06-25 11:28:23 +0530

    Add files via upload

---
+++ multimedi_app/src/components/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/video.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/audio.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/document.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File 1',
+      type: 'image',
+      path: "/file-server/image1.png"
+    },
+    {
+    id: 5,
+      name: 'Image File 2',
+      type: 'image',
+      path: "/file-server/image2.jpg"
+    },
+    {
+    id: 6,
+      name: 'Image File 3',
+      type: 'image',
+      path: "/file-server/image3.png"
+    },
+  ]
